98cd494eb747f51f96f4b623cf91be7c75ed2a6c
Jack Mac <jjspira@gmail.com>
2022-03-01 00:02:42 -0500

    tweening attempts

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,154 @@
-pub struct Tweener<T> {
-    tween: T,
+use std::{marker::PhantomData, ops::RangeInclusive};
+
+pub trait Tween<TValue, TTime>
+where
+    TValue: TweenValue,
+    TTime: TweenTime,
+{
+    fn update(&mut self, new_time: TTime) -> TValue;
 }
 
-impl<T: Tween> Tweener<T> {
-    pub fn new(tween: T) -> Self {
-        Self { tween }
-    }
-
-    pub fn run(&mut self, new_time: f32, value: f32) -> f32 {
-        self.tween.update(new_time, value)
-    }
-
-    pub fn pipe<OuterTween: Tween>(
-        self,
-        outer_tween: OuterTween,
-    ) -> Tweener<PipedTween<Self, OuterTween>> {
-        Tweener::new(PipedTween {
-            base: self,
-            outer_tween,
-        })
-    }
+#[derive(Debug)]
+pub struct Linear<TValue, TTime> {
+    range: RangeInclusive<TValue>,
+    delta: TValue,
+    duration: TTime,
 }
 
-pub trait Tween {
-    fn update(&mut self, new_time: f32, value: f32) -> f32;
-}
-
-#[derive(Debug, Default)]
-pub struct Linear {
-    start_value: f32,
-    delta: f32,
-    duration: f32,
-}
-
-impl Linear {
-    pub fn new(start: f32, end: f32, duration: f32) -> Self {
+impl<TValue, TTime> Linear<TValue, TTime>
+where
+    TValue: TweenValue,
+    TTime: TweenTime,
+{
+    pub fn new(range: RangeInclusive<TValue>, duration: TTime) -> Self {
+        let delta = TValue::calculate_delta(*range.end(), *range.start());
         Self {
-            start_value: start,
-            delta: end - start,
+            range,
+            delta,
             duration,
         }
     }
 }
 
-impl Tween for Linear {
-    fn update(&mut self, new_time: f32, _value: f32) -> f32 {
-        let percent_time = new_time / self.duration;
-        self.delta * percent_time + self.start_value
-    }
-}
-
-impl<T> Tween for T
+impl<V, T> Tween<V, T> for Linear<V, T>
 where
-    T: FnMut(f32, f32) -> f32,
+    V: TweenValue,
+    T: TweenTime,
 {
-    fn update(&mut self, new_time: f32, current_value: f32) -> f32 {
-        self(new_time, current_value)
+    fn update(&mut self, new_time: T) -> V {
+        let percent_time = T::percent(self.duration, new_time);
+        let new_value = self.delta.scale(percent_time);
+
+        new_value.add(*self.range.start())
     }
 }
 
-impl<T: Tween> Tween for Tweener<T> {
-    fn update(&mut self, new_time: f32, value: f32) -> f32 {
-        self.run(new_time, value)
+pub trait TweenValue: Copy {
+    fn calculate_delta(destination: Self, start: Self) -> Self;
+    fn add(self, other: Self) -> Self;
+    fn scale(self, scale: f32) -> Self;
+}
+
+impl TweenValue for f32 {
+    fn add(self, other: Self) -> Self {
+        self + other
+    }
+
+    fn calculate_delta(destination: Self, start: Self) -> Self {
+        destination - start
+    }
+
+    fn scale(self, scale: f32) -> Self {
+        self * scale
+    }
+}
+impl TweenValue for i32 {
+    fn add(self, other: Self) -> Self {
+        self + other
+    }
+
+    fn calculate_delta(destination: Self, start: Self) -> Self {
+        destination - start
+    }
+
+    fn scale(self, scale: f32) -> Self {
+        (self as f32 * scale) as i32
     }
 }
 
-pub struct PipedTween<BaseTween, OuterTween> {
-    base: BaseTween,
-    outer_tween: OuterTween,
+pub trait TweenTime: Copy {
+    const ZERO: Self;
+    fn percent(duration: Self, current_time: Self) -> f32;
+    fn add(self, other: Self) -> Self;
+}
+impl TweenTime for f32 {
+    const ZERO: Self = 0.0;
+    fn percent(duration: Self, current_time: Self) -> f32 {
+        current_time / duration
+    }
+
+    fn add(self, other: Self) -> Self {
+        self + other
+    }
+}
+impl TweenTime for usize {
+    const ZERO: Self = 0;
+
+    fn percent(duration: Self, current_time: Self) -> f32 {
+        current_time as f32 / duration as f32
+    }
+
+    fn add(self, other: Self) -> Self {
+        self + other
+    }
+}
+impl TweenTime for i32 {
+    const ZERO: Self = 0;
+
+    fn percent(duration: Self, current_time: Self) -> f32 {
+        current_time as f32 / duration as f32
+    }
+
+    fn add(self, other: Self) -> Self {
+        self + other
+    }
 }
 
-impl<T0, T1> Tween for PipedTween<T0, T1>
+pub struct DeltaTweener<Tween, TValue, TTime> {
+    tween: Tween,
+    last_time: TTime,
+    delta: TTime,
+    _value: PhantomData<fn(&mut Self, TTime) -> TValue>,
+}
+
+impl<T, TValue, TTime> DeltaTweener<T, TValue, TTime>
 where
-    T0: Tween,
-    T1: Tween,
+    T: Tween<TValue, TTime>,
+    TValue: TweenValue,
+    TTime: TweenTime,
 {
-    fn update(&mut self, new_time: f32, value: f32) -> f32 {
-        let value = self.base.update(new_time, value);
+    pub fn new(tween: T, delta: TTime) -> Self {
+        Self {
+            tween,
+            last_time: TTime::ZERO,
+            delta,
+            _value: PhantomData,
+        }
+    }
+}
 
-        self.outer_tween.update(new_time, value)
+impl<T, TValue, TTime> Iterator for DeltaTweener<T, TValue, TTime>
+where
+    T: Tween<TValue, TTime>,
+    TValue: TweenValue,
+    TTime: TweenTime,
+{
+    type Item = TValue;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.last_time = self.last_time.add(self.delta);
+
+        Some(self.tween.update(self.last_time))
     }
 }
 
@@ -90,38 +160,30 @@ mod tests {
     #[test]
     fn linear() {
         let mut value = 0.0;
-        let mut tweener = Tweener::new(Linear::new(value, 100.0, 10.0));
+        let mut tweener = Linear::new(value..=100.0, 10.0);
 
         for val in 1..=10 {
-            value = tweener.run(val as f32, value);
+            value = tweener.update(val as f32);
             assert_ulps_eq!(value, (val * 10) as f32);
         }
     }
 
     #[test]
-    fn closure() {
-        let mut value = 0.0;
-        let mut tweener = Tweener::new(|new_time, new_value| new_time + new_value);
+    fn linear_over_frames() {
+        let mut value = 0;
+        let mut tweener = Linear::new(value..=100, 10);
 
-        value = tweener.run(1.0, value);
-        assert_ulps_eq!(value, 1.0);
-
-        value = tweener.run(2.0, value);
-        assert_ulps_eq!(value, 3.0);
-
-        value = tweener.run(3.0, value);
-        assert_ulps_eq!(value, 6.0);
+        for val in 1..=10 {
+            value = tweener.update(val);
+            assert_eq!(value, val * 10);
+        }
     }
 
     #[test]
-    fn multiple() {
-        let mut value = 0.0;
-        let mut doubled_linear_tween =
-            Tweener::new(Linear::new(0.0, 100.0, 10.0)).pipe(|_, v| v * 2.0);
+    fn tweener() {
+        let mut value = 0;
+        let mut tweener = DeltaTweener::new(Linear::new(value..=100, 10), 1);
 
-        value = doubled_linear_tween.run(1.0, value);
-        assert_ulps_eq!(value, 20.0);
-
-        // let mut doubling_tweener = Tweener::new(|_, v| v * 2.0);
+        value = tweener.next().unwrap();
     }
 }
