b8de7ac4a6f7a2da4d49c42e39b277d1a00c1e58
Jonathan Spira <jjspira@gmail.com>
2022-12-26 19:39:45 -0800

    fixed glam

--- src/glam.rs
+++ src/glam.rs
@@ -1,14 +1,6 @@
 impl crate::TweenValue for glam::Vec2 {
     const ZERO: Self = Self::ZERO;
 
-    fn calculate_delta(destination: Self, start: Self) -> Self {
-        destination - start
-    }
-
-    fn add(self, other: Self) -> Self {
-        self + other
-    }
-
     fn scale(self, scale: f64) -> Self {
         (self.as_dvec2() * scale).as_vec2()
     }
@@ -17,14 +9,6 @@ impl crate::TweenValue for glam::Vec2 {
 impl crate::TweenValue for glam::UVec2 {
     const ZERO: Self = Self::ZERO;
 
-    fn calculate_delta(destination: Self, start: Self) -> Self {
-        destination - start
-    }
-
-    fn add(self, other: Self) -> Self {
-        self + other
-    }
-
     fn scale(self, scale: f64) -> Self {
         (self.as_dvec2() * scale).as_uvec2()
     }
@@ -32,14 +16,6 @@ impl crate::TweenValue for glam::UVec2 {
 
 impl crate::TweenValue for glam::DVec2 {
     const ZERO: Self = Self::ZERO;
-
-    fn calculate_delta(destination: Self, start: Self) -> Self {
-        destination - start
-    }
-
-    fn add(self, other: Self) -> Self {
-        self + other
-    }
 
     fn scale(self, scale: f64) -> Self {
         self * scale
