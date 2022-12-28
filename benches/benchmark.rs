ca7d2644ddc48a9e26984482f638b241a69d4e14
Jonathan Spira <jjspira@gmail.com>
2022-12-27 16:03:51 -0800

    new bench

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,18 +1,28 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use tween::Tweener;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use tween::{BounceIn, FixedTweener, Tweener};
 
-fn func() {
-    let my_boys: [_; 10] = std::array::from_fn(|_| Tweener::bounce_in(0.0f32, 10.0, 3.0f32).into_fixed(1.0));
-
-    for mut my_boy in my_boys {
-        for _ in 0..3 {
-            my_boy.move_next();
-        }
+fn func(i: &mut [FixedTweener<f32, f32, BounceIn>]) {
+    for tweener in i.iter_mut() {
+        tweener.move_next();
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("handle 10", |b| b.iter(func));
+    let mut input: Vec<_> = (0..1048576)
+        .map(|_| Tweener::bounce_in(0.0, 10.0, 10.0).into_fixed(1.0))
+        .collect();
+
+    c.bench_function("simd come thru", |b| {
+        for v in input.iter_mut() {
+            v.move_to(0.0);
+        }
+
+        b.iter(|| {
+            func(&mut input);
+        });
+
+        black_box(&input);
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
