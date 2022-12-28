ed207103cd2901126c5379883fb57d5576e835ca
Jonathan Spira <jjspira@gmail.com>
2022-12-27 16:13:51 -0800

    oh yeah

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,22 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use tween::{BounceIn, FixedTweener, Tweener};
+use tween::{BounceIn, Tweener};
 
-fn func(i: &mut [FixedTweener<f32, f32, BounceIn>]) {
+fn func(i: &mut [Tweener<f32, f32, BounceIn>]) {
     for tweener in i.iter_mut() {
-        tweener.move_next();
+        tweener.move_to(5.5);
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut input: Vec<_> = (0..1048576)
-        .map(|_| Tweener::bounce_in(0.0, 10.0, 10.0).into_fixed(1.0))
-        .collect();
+    let mut input: Vec<_> = (0..104857006).map(|_| Tweener::bounce_in(0.0, 10.0, 10.0)).collect();
 
     c.bench_function("simd come thru", |b| {
-        for v in input.iter_mut() {
-            v.move_to(0.0);
-        }
-
         b.iter(|| {
             func(&mut input);
         });
